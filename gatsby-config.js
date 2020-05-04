1dabab65481f93a974f5d280bd79626605437cd6
Kevin <kevin.m.guertin@gmail.com>
2020-05-03 23:07:52 -0400

    working on graphql portion

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,8 +11,8 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        path: `${__dirname}/src/pages`,
-        name: pages
+        name: `markdown-pages`,
+        path: `${__dirname}/src/pages/markdown-pages`,
       },
     },
     `gatsby-transformer-sharp`,
